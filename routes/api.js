e6c4d82ad1ced9108e03002f01f6fdaeec54fabd
yuvra <yuvraj.mane@yahoo.com>
2019-09-16 19:15:15 +0530

    UODATE

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,44 @@
 const expres = require("express");
 const router = expres.Router();
+const Profile = require('../models/profile');
 
+router.get('/login', (req,res) => {
+    const Auth = {
+        token: "mtGDyr3rFW"
+    }
+    res.send(Auth);
+});
 
 router.get('/data', (req,res) => {
-    res.send({type: 'GET'});
+    if(req) {
+        console.log("************* GET REQ", req.body);
+    }
+    Profile.find().then((data)=>{
+        res.send(data);
+    });
+    // res.send({type: 'GET'});
 });
 
 router.post('/data', (req,res) => {
-    console.log(req.body); 
-    res.send( 
-        [
-            { type: 'POST' }, 
-            { data: req.body } 
-        ]
-    );
+    
+    Profile.create(req.body).then((profile) => { 
+        res.send(profile);
+    });
 });
 
 router.put('/data/:id', (req,res) => {
-    res.send({ type: 'PUT'});
+    Profile.findByIdAndUpdate({_id: req.params.id}, req.body).then(()=>{
+        res.send({ type: 'PUT'});
+    })
+    
 });
 
 router.delete('/data/:id', (req,res) => {
-    res.send({ type: 'DELETE'});
+    console.log("Del id",req.params.id); 
+    Profile.findByIdAndDelete(({_id:req.params.id})).then((re)=> {
+        res.send({ type: re});
+    });
+    
 });
 
 module.exports = router;
